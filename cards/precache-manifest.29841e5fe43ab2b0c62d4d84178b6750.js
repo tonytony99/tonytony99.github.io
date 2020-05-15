ffb51921cd9e818f0fbad07e61186aed9c0fc019
tony <anthony.bryan@outlook.com>
2020-05-15 19:48:54 +0100

    add three is agree rule and fix issue with angle

---
+++ cards/precache-manifest.29841e5fe43ab2b0c62d4d84178b6750.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ceadc61ce437d2d2629355f64515a1a",
+    "url": "/cards/index.html"
+  },
+  {
+    "revision": "4a27b8b22ae47e94d738",
+    "url": "/cards/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "f797d8bb893a3372f5f7",
+    "url": "/cards/static/css/main.24272e4a.chunk.css"
+  },
+  {
+    "revision": "4a27b8b22ae47e94d738",
+    "url": "/cards/static/js/2.ad8a40c3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/cards/static/js/2.ad8a40c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f797d8bb893a3372f5f7",
+    "url": "/cards/static/js/main.c5eefbc1.chunk.js"
+  },
+  {
+    "revision": "0ba09f66b56cbf03340e",
+    "url": "/cards/static/js/runtime-main.fc35caea.js"
+  }
+]);
