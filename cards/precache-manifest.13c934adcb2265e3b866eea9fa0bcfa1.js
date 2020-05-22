d94a8e311bfb61de6c28a7dcf506152fd1eb8f42
tony <anthony.bryan@outlook.com>
2020-05-23 00:42:42 +0100

    change rules

---
+++ cards/precache-manifest.13c934adcb2265e3b866eea9fa0bcfa1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "453e5d9ad0e0bb6cf4eedcd9d81bc5ff",
+    "url": "/cards/index.html"
+  },
+  {
+    "revision": "4a27b8b22ae47e94d738",
+    "url": "/cards/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "77a4b5fb6c754e671826",
+    "url": "/cards/static/css/main.24272e4a.chunk.css"
+  },
+  {
+    "revision": "4a27b8b22ae47e94d738",
+    "url": "/cards/static/js/2.ad8a40c3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/cards/static/js/2.ad8a40c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77a4b5fb6c754e671826",
+    "url": "/cards/static/js/main.dc5a6600.chunk.js"
+  },
+  {
+    "revision": "0ba09f66b56cbf03340e",
+    "url": "/cards/static/js/runtime-main.fc35caea.js"
+  }
+]);
