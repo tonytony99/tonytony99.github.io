35c1ecf37fada99c89c986f47a6093a4c6d96f22
tony <anthony.bryan@outlook.com>
2020-06-12 19:40:05 +0100

    add upgrades to game

---
+++ roc/precache-manifest.09ad0c91f5e66c9db4f99f9aac4698d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17bbe7c70290aee3d73bb6538ccd945b",
+    "url": "/roc/index.html"
+  },
+  {
+    "revision": "290fafb06fcccbb3f40c",
+    "url": "/roc/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6c8b39ab70f47e8f807e",
+    "url": "/roc/static/css/main.349462eb.chunk.css"
+  },
+  {
+    "revision": "290fafb06fcccbb3f40c",
+    "url": "/roc/static/js/2.ba702916.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/roc/static/js/2.ba702916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c8b39ab70f47e8f807e",
+    "url": "/roc/static/js/main.f797254b.chunk.js"
+  },
+  {
+    "revision": "ccd5db572707bdf7365c",
+    "url": "/roc/static/js/runtime-main.8fc1b5c8.js"
+  }
+]);
