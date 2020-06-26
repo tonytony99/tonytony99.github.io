59ab928676ea3eeab5e25fbfd872cc811dfa6770
tony <anthony.bryan@outlook.com>
2020-06-26 17:42:25 +0100

    fixed polling bug

---
+++ roc/precache-manifest.fa0a6e58d9b79b12b11c20fa582dff7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be70fee9d5086c79b6e5d6c83a6d2e5",
+    "url": "/roc/index.html"
+  },
+  {
+    "revision": "316ceb1dc1a5d53ae896",
+    "url": "/roc/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "38833847cbeaade16127",
+    "url": "/roc/static/css/main.349462eb.chunk.css"
+  },
+  {
+    "revision": "316ceb1dc1a5d53ae896",
+    "url": "/roc/static/js/2.375748ea.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/roc/static/js/2.375748ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38833847cbeaade16127",
+    "url": "/roc/static/js/main.92a1c5db.chunk.js"
+  },
+  {
+    "revision": "ccd5db572707bdf7365c",
+    "url": "/roc/static/js/runtime-main.8fc1b5c8.js"
+  }
+]);
--- roc/service-worker.js
+++ roc/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/roc/precache-manifest.f3fdb756953d84340786ab4d56812ada.js"
+  "/roc/precache-manifest.fa0a6e58d9b79b12b11c20fa582dff7f.js"
 );
 
 self.addEventListener('message', (event) => {
